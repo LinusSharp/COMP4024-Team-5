9f011b0e6a67dcb7d57069f9b7d01bb6a2eb8ecd
tbrad <psytb10@nottingham.ac.uk>
2025-02-21 15:04:01 +0000

    fix: fix bug when jumping on obstacles

--- COMP4024-Team5/Assets/Scripts/Player/PlayerControllerSideView.cs
+++ COMP4024-Team5/Assets/Scripts/Player/PlayerControllerSideView.cs
@@ -5,44 +5,40 @@ public class PlayerControllerSideView : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
     
+    [SerializeField]
     private Rigidbody2D rb;
-    public bool isGrounded = false;
-
+    [SerializeField]
+    private Collider2D floorBoxCollider;
+    private bool _jumpRequested;
+    
     private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    private void Update()
-    {
-        // only move left or right
-        float moveX = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(moveX * moveSpeed, rb.linearVelocity.y);
-
-        // this is jump
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-        }
-    }
-
-    // Checks if the player is on the ground or not
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.contacts[0].normal.y > 0.5f)
-        {
-            isGrounded = true;
-        }
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        isGrounded = false;
-    }
-
-    // this is so that there is gravity or not
-    private void OnEnable()
     {
         if (rb == null) rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 1;
+    }
+    
+    private void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && IsGrounded())
+        {
+            _jumpRequested = true; // Flag for jumping, applied in FixedUpdate
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rb.linearVelocity.y);
+
+        if (_jumpRequested)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            _jumpRequested = false;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        var bounds = floorBoxCollider.bounds;
+        return Physics2D.OverlapBox(bounds.center, bounds.size, 0f, LayerMask.GetMask("Ground"));
     }
 }
