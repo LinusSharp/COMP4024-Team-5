b950aa6ea87dec37f1f5876fd117c035473fce28
Selles Tinhinane <sellestinhinane@Selless-Laptop.local>
2025-02-22 13:04:50 +0000

    fix/ fix code to avoide multiple triggers

--- COMP4024-Team5/Assets/Scripts/Item/ItemController.cs
+++ COMP4024-Team5/Assets/Scripts/Item/ItemController.cs
@@ -3,24 +3,28 @@ using UnityEngine.SceneManagement;
 public class ItemController : MonoBehaviour
 {
     [SerializeField] private string nextScene = "Lobby";
-    private void Start()
+    private bool itemCollected = false; // boolean variable to avoid mutliple triggers. 
+   
+    private void Start() // debugging 
     {
         Debug.Log("CollectableItem Loaded in Scene: " + SceneManager.GetActiveScene().name);
         gameObject.SetActive(true);
     }
     
     
-    private void OnDestroy()
+    
+    private void OnDestroy() // debugging
     {
-        Debug.Log("CollectableItem has been destroyed!");
+        Debug.Log("CollectableItem has been destroyed");
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Collected Item! Loading Lobby...");
+        if (other.CompareTag("Player") && !itemCollected)
+        { 
+            itemCollected = true;
+            Debug.Log("Collected Item");
 
-           
+            gameObject.SetActive(false); // avoiding multiple triggers
             SceneManager.LoadScene(nextScene);
         }
     }
