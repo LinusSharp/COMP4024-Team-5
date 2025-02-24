8b5c9a5902c93f4e3edf38bb5d1410e4e8be0046
Linus Sharp <linussharp@Linuss-MacBook-Pro.local>
2025-02-24 16:18:14 +0000

    feat: added a winning screen

--- COMP4024-Team5/Assets/Scripts/Item/ItemController.cs
+++ COMP4024-Team5/Assets/Scripts/Item/ItemController.cs
@@ -7,27 +7,13 @@ public class ItemController : MonoBehaviour
     [SerializeField] private GameObject itemCollectedUI;
     [SerializeField] private string itemKey; 
     
-    private bool itemCollected = false; // Boolean variable to avoid multiple triggers.
+    private bool itemCollected = false;
 
     private void Start() 
     {
         Debug.Log("Scene : " + SceneManager.GetActiveScene().name);
 
-        // hiding the item if it is already collecting 
-        if (CollectionController.Instance.IsItemCollected(itemKey))
-        {
-            Debug.Log($"Item {itemKey} already collected, hiding it.");
-            gameObject.SetActive(false); 
-            return; 
-        }
-
         gameObject.SetActive(true);
-
-        // hiding the UI before we collect the item
-        if (itemCollectedUI != null)
-        {
-            itemCollectedUI.SetActive(false);
-        }
     }
 
     private void OnDestroy() 
@@ -40,30 +26,15 @@ public class ItemController : MonoBehaviour
         if (other.CompareTag("Player") && !itemCollected)
         {
             itemCollected = true;
-            Debug.Log("Collected Item: " + itemKey); // Debugging
+            Debug.Log("Collected Item: " + itemKey);
 
-            gameObject.SetActive(false); // avoiding multiple triggers and hiding the item after collection.
-
+            gameObject.SetActive(false);
             PlayerController player = other.gameObject.GetComponent<PlayerController>();
             if (player != null) {
                 player.IncreaseLevel();
                 Destroy(gameObject);
                 BackToLobby();
             }
-
-            //  track collected items
-            //CollectionController.Instance.CollectItem(itemKey); 
-            
-            // saving the collected item 
-            //PlayerPrefs.SetInt(itemKey, 1);
-            //PlayerPrefs.Save();
-			
-        
-
-            //if (itemCollectedUI != null)
-            //{
-               // itemCollectedUI.SetActive(true);
-            //}
         }
     }
     public void BackToLobby()
